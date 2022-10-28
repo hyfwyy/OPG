5afa1ae99e056648797cab3fb903899aa6f25c57
hyfwyy <329894031@qq.com>
2022-10-28 04:27:11 +0000

    10/28/22

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-git remote add origin git@github.com:hyfwyy/PICM.git
+# git remote add origin git@github.com:hyfwyy/PICM.git
 git add .
 git commit -m "`date '+%D'`"
 git push origin main
